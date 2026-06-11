bb4890d952a9c8a656fced0b0f4e017d3985100a
raczk0wski <raczkowski.bartek@gmail.com>
2026-06-11 21:29:07 +0200

    fix

--- src/main/java/com/raczkowski/app/article/ArticleRepository.java
+++ src/main/java/com/raczkowski/app/article/ArticleRepository.java
@@ -6,6 +6,7 @@ import lombok.NonNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,7 +18,7 @@ import java.util.List;
 
 @Repository
 @Transactional(readOnly = true)
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long>, JpaSpecificationExecutor<Article> {
     @NonNull Page<Article> findAll(@NonNull Pageable pageable);
 
     @Query("SELECT a FROM Article a ORDER BY a.isPinned DESC")
